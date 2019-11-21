9cb64f69815965c7b2e71c2fb69b2bb690797608
csterling <coreytsterling@gmail.com>
2019-11-22 09:45:49 +1300

    Added --no-images flag to ADAMS report writer, to just output the reports. Possibly useful if the images are already present from the input format.

--- src/wai/annotations/io/_adams/_ADAMSReportWriter.py
+++ src/wai/annotations/io/_adams/_ADAMSReportWriter.py
@@ -1,5 +1,6 @@
 import os
-from typing import Iterable
+from argparse import ArgumentParser, Namespace
+from typing import Iterable, Any, Dict
 
 from wai.common.file.report import save
 
@@ -9,6 +10,24 @@ from .constants import EXTENSION
 
 
 class ADAMSReportWriter(Writer[ADAMSExternalFormat]):
+    def __init__(self, output: str, no_images: bool = False):
+        super().__init__(output)
+
+        self.no_images: bool = no_images
+
+    @classmethod
+    def configure_parser(cls, parser: ArgumentParser):
+        super().configure_parser(parser)
+
+        parser.add_argument("--no-images", action="store_true", required=False, dest="no_images",
+                            help="skip the writing of images, outputting only the report files")
+
+    @classmethod
+    def determine_kwargs_from_namespace(cls, namespace: Namespace) -> Dict[str, Any]:
+        kwargs = super().determine_kwargs_from_namespace(namespace)
+        kwargs.update(no_images=namespace.no_images)
+        return kwargs
+
     @classmethod
     def output_help_text(cls) -> str:
         return "output directory to write files to"
@@ -23,9 +42,10 @@ class ADAMSReportWriter(Writer[ADAMSExternalFormat]):
             # Unpack the instance
             image_filename, image_data, image_filetype, report = instance
 
-            # Write the file
-            with open(os.path.join(path, image_filename), "wb") as file:
-                file.write(image_data)
+            # Write the image
+            if not self.no_images:
+                with open(os.path.join(path, image_filename), "wb") as file:
+                    file.write(image_data)
 
             # Format the report filename
             report_filename = os.path.splitext(image_filename)[0] + EXTENSION
