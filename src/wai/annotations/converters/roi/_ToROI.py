0bd78779664d106def95cc14560d1ca546bc5e79
csterling <coreytsterling@gmail.com>
2019-12-04 12:08:02 +1300

    Fixed normalised coordinate calculation. Added option to specify global file dimensions for cases where they can't be inferred from the input files.

--- src/wai/annotations/converters/roi/_ToROI.py
+++ src/wai/annotations/converters/roi/_ToROI.py
@@ -1,4 +1,5 @@
-from typing import Dict, Optional, List, Pattern
+from argparse import ArgumentParser, Namespace
+from typing import Dict, Optional, List, Pattern, Tuple, Any
 
 from wai.common.adams.imaging.locateobjects import LocatedObjects, LocatedObject
 
@@ -11,21 +12,61 @@ class ToROI(InternalFormatConverter[ROIExternalFormat]):
     """
     Converter from internal format to ROI annotations.
     """
-    def __init__(self, labels: Optional[List[str]] = None, regex: Optional[Pattern] = None):
+    def __init__(self,
+                 labels: Optional[List[str]] = None,
+                 regex: Optional[Pattern] = None,
+                 image_size: Optional[Tuple[int, int]] = None
+                 ):
         super().__init__(labels, regex)
 
+        self.image_size: Optional[Tuple[int, int]] = image_size
+
         self._label_map: Dict[str, int] = {}
+
+    @classmethod
+    def configure_parser(cls, parser: ArgumentParser):
+        super().configure_parser(parser)
+
+        parser.add_argument(
+            "-d", "--image-dimensions", metavar="width,height", dest="image_size", required=False,
+            help="image dimesions to use if none can be inferred",
+            default="")
+
+    @classmethod
+    def determine_kwargs_from_namespace(cls, namespace: Namespace) -> Dict[str, Any]:
+        kwargs = super().determine_kwargs_from_namespace(namespace)
+        kwargs.update(
+            image_size=tuple(map(int, namespace.image_size.split(","))) if len(namespace.image_size) > 0 else None
+        )
+        return kwargs
 
     def convert_unpacked(self,
                          image_info: ImageInfo,
                          located_objects: LocatedObjects) -> ROIExternalFormat:
-        return image_info, list(map(self.convert_located_object, located_objects))
+        # Make sure we can get the image size
+        if image_info.size is not None:
+            image_size = image_info.size
+        elif self.image_size is not None:
+            image_size = self.image_size
+        else:
+            raise RuntimeError(f"Cannot infer image dimensions for {image_info.filename}")
 
-    def convert_located_object(self, located_object: LocatedObject) -> ROIObject:
+        # Create a wrapper to convert located objects using the image size
+        def convert(located_object: LocatedObject) -> ROIObject:
+            return self.convert_located_object(located_object, *image_size)
+
+        return image_info, list(map(convert, located_objects))
+
+    def convert_located_object(self,
+                               located_object: LocatedObject,
+                               image_width: int,
+                               image_height: int) -> ROIObject:
         """
         Converts a located object to ROI format.
 
         :param located_object:  The located object.
+        :param image_width:     The width of the image.
+        :param image_height:    The height of the image.
         :return:                The ROI object.
         """
         # Get the object's rectangle
@@ -42,10 +83,10 @@ class ToROI(InternalFormatConverter[ROIExternalFormat]):
                          float(rectangle.top()),
                          float(rectangle.right()),
                          float(rectangle.bottom()),
-                         rectangle.left() / rectangle.width(),
-                         rectangle.top() / rectangle.height(),
-                         rectangle.right() / rectangle.width(),
-                         rectangle.bottom() / rectangle.height(),
+                         rectangle.left() / image_width,
+                         rectangle.top() / image_height,
+                         rectangle.right() / image_width,
+                         rectangle.bottom() / image_height,
                          self._label_map[label],
                          label,
                          1.0)
