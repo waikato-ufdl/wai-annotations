e58a19ac93bc8d08bf54ba5e0c1b8db12cec84a9
csterling <coreytsterling@gmail.com>
2020-01-23 09:45:06 +1300

    Made format availability checking more descriptive.

--- src/wai/annotations/main/_components.py
+++ src/wai/annotations/main/_components.py
@@ -50,19 +50,33 @@ def get_formats() -> Set[str]:
     return set(components.keys())
 
 
-def ensure_format(format: str):
+def ensure_format(format: str, input: Optional[bool] = None):
     """
     Raises an error if the format is not known.
 
     :param format:  The format to check.
+    :param input:   True to check the format can be read,
+                    False to check the format can be written,
+                    None (the default) to check both.
     """
     # Get the formats
     formats = get_formats()
 
-    # Check the given format
+    # Check the given format exists at all
     if format not in formats:
-        raise ValueError(f"Unknown format '{format}'\n"
-                         f"Available formats are {formats}")
+        raise ValueError(f"Unknown format '{format}'. Available formats are {formats}")
+
+    # Get the components to check for existence
+    components_to_check = (components[format] if input is None else
+                           components[format][:2] if input else
+                           components[format][2:])
+
+    if any(component is None for component in components_to_check):
+        check_string = ("readable/writable" if input is None else
+                        "readable" if input else
+                        "writable")
+        raise ValueError(f"Format '{format}' is not currently implemented as a "
+                         f"{check_string} format")
 
 
 def get_reader_factory(format: str) -> Optional[Type[CommandLineReaderFactory]]:
@@ -73,7 +87,7 @@ def get_reader_factory(format: str) -> Optional[Type[CommandLineReaderFactory]]:
     :return:        The reader factory for the format.
     """
     # Check the format is known
-    ensure_format(format)
+    ensure_format(format, True)
 
     # Get the reader factory for the format
     return components[format][0]
@@ -87,7 +101,7 @@ def get_external_format_converter_factory(format: str) -> Optional[Type[CommandL
     :return:        The external format converter factory for the format.
     """
     # Check the format is known
-    ensure_format(format)
+    ensure_format(format, True)
 
     # Get the external format converter factory for the format
     return components[format][1]
@@ -101,7 +115,7 @@ def get_internal_format_converter_factory(format: str) -> Optional[Type[CommandL
     :return:        The internal format converter factory for the format.
     """
     # Check the format is known
-    ensure_format(format)
+    ensure_format(format, False)
 
     # Get the internal format converter factory for the format
     return components[format][2]
@@ -115,7 +129,7 @@ def get_writer_factory(format: str) -> Optional[Type[CommandLineWriterFactory]]:
     :return:        The writer factory for the format.
     """
     # Check the format is known
-    ensure_format(format)
+    ensure_format(format, False)
 
     # Get the reader factory for the format
     return components[format][3]
