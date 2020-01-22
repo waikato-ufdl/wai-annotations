0b87e85032107ec17bb6201712495e5b5ef82f5c
csterling <coreytsterling@gmail.com>
2020-01-23 09:32:23 +1300

    Added optional predicate for controlling which stream elements to log to StreamLogger.

--- src/wai/annotations/core/logging/_StreamLogger.py
+++ src/wai/annotations/core/logging/_StreamLogger.py
@@ -5,19 +5,25 @@ from .._InlineStreamProcessor import InlineStreamProcessor, StreamElementType
 
 class StreamLogger(InlineStreamProcessor[StreamElementType]):
     """
-    Stream processor that logs a message for each item in the stream.
+    Stream processor that logs a message for each item in the stream,
+    subject to a predicate.
     """
     def __init__(self,
                  logging_method: Callable[[str], None],
-                 message_formatter: Callable[[StreamElementType], str]):
+                 message_formatter: Callable[[StreamElementType], str],
+                 predicate: Callable[[StreamElementType], bool] = lambda stream_element: True):
         # Logger method to use to log the message (e.g. logger.info)
         self._logging_method: Callable[[str], None] = logging_method
 
         # Function to convert a stream element into a log message (e.g. lambda e: f"Processing element: {e.name}")
         self._message_formatter: Callable[[StreamElementType], str] = message_formatter
 
+        # Predicate which decides which elements should be logged
+        self._predicate = predicate
+
     def _process_element(self, element: StreamElementType) -> Iterable[StreamElementType]:
         # Log the message
-        self._logging_method(self._message_formatter(element))
+        if self._predicate(element):
+            self._logging_method(self._message_formatter(element))
 
         return element,
