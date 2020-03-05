ddc0f7a6452b0ac429461a764dd50d0d168af83b
csterling <coreytsterling@gmail.com>
2020-03-06 08:51:09 +1300

    Added pretty option to VGGWriter.

--- src/wai/annotations/core/cli/_CommandLineVGGWriterFactory.py
+++ src/wai/annotations/core/cli/_CommandLineVGGWriterFactory.py
@@ -1,4 +1,5 @@
-from typing import Type
+from argparse import ArgumentParser, Namespace
+from typing import Type, Dict, Any
 
 from ...core.cli import CommandLineSeparateImageWriterFactory
 from ...core import SeparateImageWriter
@@ -16,3 +17,18 @@ class CommandLineVGGWriterFactory(CommandLineSeparateImageWriterFactory):
     @classmethod
     def output_help_text(cls) -> str:
         return "output file to write annotations to (images are placed in same directory)"
+
+    @classmethod
+    def configure_parser(cls, parser: ArgumentParser):
+        super().configure_parser(parser)
+
+        parser.add_argument("--pretty", action="store_true", dest="pretty",
+                            help="whether to pretty-print the output")
+
+    @classmethod
+    def determine_kwargs_from_namespace(cls, namespace: Namespace) -> Dict[str, Any]:
+        kwargs = super().determine_kwargs_from_namespace(namespace)
+
+        kwargs.update(pretty=namespace.pretty)
+
+        return kwargs
--- src/wai/annotations/vgg/io/_VGGWriter.py
+++ src/wai/annotations/vgg/io/_VGGWriter.py
@@ -9,6 +9,11 @@ class VGGWriter(SeparateImageWriter[VGGExternalFormat]):
     """
     Writer of VGG-format JSON files.
     """
+    def __init__(self, output: str, no_images: bool = False, pretty: bool = False):
+        super().__init__(output, no_images)
+
+        self._pretty: bool = pretty
+
     def write_without_images(self, instances: Iterable[VGGExternalFormat], path: str):
         # Create a map of images
         images = {}
@@ -22,7 +27,7 @@ class VGGWriter(SeparateImageWriter[VGGExternalFormat]):
             images[f"{image.filename}-1"] = image
 
         # Save the file
-        VGGFile(**images).save_json_to_file(path)
+        VGGFile(**images).save_json_to_file(path, 2 if self._pretty else None)
 
     def extract_image_info_from_external_format(self, instance: VGGExternalFormat) -> ImageInfo:
         # Unpack the instance
