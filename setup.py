3927f958402a51ab5d63d4ec296e56cb2960d1b8
csterling <coreytsterling@gmail.com>
2020-03-18 13:03:11 +1300

    Pre-release.

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     namespace_packages=[
         "wai"
     ],
-    version="0.3.0",
+    version="0.3.1",
     author='Corey Sterling',
     author_email='coreytsterling@gmail.com',
     install_requires=[
