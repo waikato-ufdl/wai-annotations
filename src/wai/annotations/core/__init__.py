e0197551d23d63d492e2a995131ee4dcab15c719
csterling <coreytsterling@gmail.com>
2020-04-02 11:24:39 +1300

    Added missing public import for InputChain.

--- src/wai/annotations/core/__init__.py
+++ src/wai/annotations/core/__init__.py
@@ -4,6 +4,7 @@ Package for core functionality common to all conversions.
 from ._FormatSpecifier import FormatSpecifier
 from ._ImageFormat import ImageFormat
 from ._ImageInfo import ImageInfo
+from ._InputChain import InputChain
 from ._InternalFormat import InternalFormat
 from ._LibrarySettings import LibrarySettings, get_settings, set_settings
 from ._OutputChain import OutputChain
