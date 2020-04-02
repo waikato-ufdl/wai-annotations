490364d50fa4858d4a61b84eac61a354a3456a73
csterling <coreytsterling@gmail.com>
2020-04-03 09:31:30 +1300

    Added interface to writer's file-iterator method.

--- src/wai/annotations/core/_OutputChain.py
+++ src/wai/annotations/core/_OutputChain.py
@@ -1,4 +1,4 @@
-from typing import Iterable, Tuple
+from typing import Iterable, Tuple, Iterator, IO
 
 from .components import InternalFormatConverter, Writer
 from .stream import InlineStreamProcessor
@@ -48,3 +48,20 @@ class OutputChain:
             instances = pre_processor.process(instances)
 
         self._writer.save(self._converter.convert_all(instances))
+
+    def file_iterator(self, instances: Iterable[InternalFormat]) -> Iterator[Tuple[str, IO[bytes]]]:
+        """
+        Converts a series of instances into the files they are written to.
+        No files are actually written.
+
+        N.B. Some files are written, but to a temporary directory which is
+        removed on iterator completion.
+
+        :param instances:   The instances to write.
+        :return:            An iterator of filename, file-contents pairs.
+        """
+        # Apply the pre-processing
+        for pre_processor in reversed(self._pre_processors):
+            instances = pre_processor.process(instances)
+
+        return self._writer.file_iterator(self._converter.convert_all(instances))
