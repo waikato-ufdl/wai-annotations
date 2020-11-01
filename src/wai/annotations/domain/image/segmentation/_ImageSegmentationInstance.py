2e5377c1ff7d2865fd88a207164add4a10c1e73b
csterling <coreytsterling@gmail.com>
2020-11-02 10:41:31 +1300

    Fixed bug where negative instances were still trying to check annotations.

--- src/wai/annotations/domain/image/segmentation/_ImageSegmentationInstance.py
+++ src/wai/annotations/domain/image/segmentation/_ImageSegmentationInstance.py
@@ -11,7 +11,7 @@ class ImageSegmentationInstance(ImageInstance[ImageSegmentationAnnotation]):
     """
     def __init__(self, data: Image, annotations: Optional[ImageSegmentationAnnotation]):
         # Make sure the data and annotation images are the same size
-        if data.size != annotations.size:
+        if annotations is not None and data.size != annotations.size:
             raise Exception(f"Image size {data.size} does not match segments size {annotations.size}")
 
         super().__init__(data, annotations)
