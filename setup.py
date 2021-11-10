ed5efceba8f34850a8e7c3f11af9b64474b7b4f8
csterling <coreytsterling@gmail.com>
2021-11-11 10:24:09 +1300

    Added explicit dependency on wai.annotations.core to ensure latest functionality.

--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ setup(
     author='Corey Sterling',
     author_email='coreytsterling@gmail.com',
     install_requires=[
+        "wai.annotations.core>=0.1.4",
         "wai.annotations.adams>=1.0.1",
         "wai.annotations.bluechannel>=1.0.1",
         "wai.annotations.coco>=1.0.1",
