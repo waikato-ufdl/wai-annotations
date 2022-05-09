82759873b4b8f859df8369070e61ce9e12112fcd
Peter Reutemann <fracpete@gmail.com>
2022-05-09 15:26:05 +1200

    updated version for wai.annotations.imgstats

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
         "wai.annotations.commonvoice>=1.0.1",
         "wai.annotations.festvox>=1.0.0",
         "wai.annotations.imgaug>=1.0.3",
-        "wai.annotations.imgstats>=1.0.1",
+        "wai.annotations.imgstats>=1.0.2",
         "wai.annotations.indexedpng>=1.0.0",
         "wai.annotations.layersegments>=1.0.0",
         "wai.annotations.roi>=1.0.0",
