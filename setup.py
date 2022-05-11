e3f6b07b96ea8edeecf7e3b64aa82f1c5e657508
Peter Reutemann <fracpete@gmail.com>
2022-05-11 15:37:09 +1200

    incremented layersegments dependency to 1.0.1

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
         "wai.annotations.imgstats>=1.0.2",
         "wai.annotations.imgvis>=1.0.1",
         "wai.annotations.indexedpng>=1.0.0",
-        "wai.annotations.layersegments>=1.0.0",
+        "wai.annotations.layersegments>=1.0.1",
         "wai.annotations.opex>=1.0.0",
         "wai.annotations.roi>=1.0.0",
         "wai.annotations.subdir>=1.0.0",
