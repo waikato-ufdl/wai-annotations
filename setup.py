72fda0f025f69b4f52d5ccee2b02b337cdb2b48d
Peter Reutemann <fracpete@gmail.com>
2022-05-12 14:22:31 +1200

    upgraded imgaug dependency to 1.0.4

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
         "wai.annotations.coco>=1.0.1",
         "wai.annotations.commonvoice>=1.0.1",
         "wai.annotations.festvox>=1.0.0",
-        "wai.annotations.imgaug>=1.0.3",
+        "wai.annotations.imgaug>=1.0.4",
         "wai.annotations.imgstats>=1.0.2",
         "wai.annotations.imgvis>=1.0.2",
         "wai.annotations.indexedpng>=1.0.0",
