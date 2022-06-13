3bb38661812e2e071ae3b7145bc663847a63746e
Peter Reutemann <fracpete@gmail.com>
2022-06-13 15:39:30 +1200

    upgraded wai.annotations.grayscale to 1.0.1

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
         "wai.annotations.coco>=1.0.2",
         "wai.annotations.commonvoice>=1.0.1",
         "wai.annotations.festvox>=1.0.0",
-        "wai.annotations.grayscale>=1.0.0",
+        "wai.annotations.grayscale>=1.0.1",
         "wai.annotations.imgaug>=1.0.5",
         "wai.annotations.imgstats>=1.0.3",
         "wai.annotations.imgvis>=1.0.3",
